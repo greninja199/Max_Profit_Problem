0b2688ee3f187bd380569664bbb275d66465d08c
greninja199 <38431612+greninja199@users.noreply.github.com>
2019-04-14 13:37:38 +0530

    Update bottomTop.cpp

--- bottomTop.cpp
+++ bottomTop.cpp
@@ -15,18 +15,9 @@ int main()
     for(int k=j;k<n;k++)
       a[i][k]=0;
   }
-  /*for(int i=0;i<n;i++)
-  {
-    cout<<endl;
-    for(j=0;j<n;j++)
-      cout<<a[i][j]<<"\t";
-  }*/
   int arr[n];
   for(int i=0;i<n;i++)
     arr[i]=a[n-1][i];
-  cout<<endl;
-  //for(int i=0;i<n;i++)
-    //cout<<arr[i]<<"\t";
   for(int i=n-2;i>=0;i--)
   {
     for(j=0;j<=i;j++)
@@ -37,5 +28,5 @@ int main()
         arr[j]=arr[j+1]+a[i][j];
     }
   }
-  cout<<endl<<arr[0];
+  cout<<arr[0];
 }
